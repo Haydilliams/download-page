63b944d419584daa8aecd56acd03c31580568127
Hayden Williams <haydilliams@gmail.com>
2020-08-08 07:44:08 -0400

    Added class for easy reuse and encapsulation of images

--- src/PhotoBlock.js
+++ src/PhotoBlock.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
+import './App.scss';
 
 class PhotoBlock extends Component {
     constructor(props) {
         super(props);
-        this.state = {image: 'image link', imageName: 'imageName'}
     }
 
     render() {
+
+        // Rather than importing every single image, this is a nice one compact solution.
+        // kudos to: https://stackoverflow.com/questions/42118296/dynamically-import-images-from-a-directory-using-webpack
+
+        function importAll(r) {
+            let images = {};
+            r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+            return images;
+        }
+
+        const images = importAll(require.context('./images', false, /\.(jpe?g)$/));
+        var imagePath = this.props.imageName + "500.jpg";
+        var readableImageName = this.props.imageName.replace(/_/g, " ");
+
         return (
-            <div>
-                <div className='photo-cell'>
+            <div className='photo-wrapper'>
+                <div className='photo-background'>
+                    <img src={images[imagePath]} alt={readableImageName} /> 
                 </div>
-                <p>{this.state.image}</p>
-                <p>{this.state.imageName}</p>
+                <div className ='photo-text'>{readableImageName}</div>
+                
             </div>
         );
     }
 }
- 
+
 export default PhotoBlock;
