c4e6becc10518412b2687b84f5330fede9f85dac
Hayden Williams <haydilliams@gmail.com>
2020-09-09 13:53:05 -0400

    Made build folder public in order for elastic beanstalk to access it

---
+++ frontend/build/precache-manifest.0574689b2130f892d2e08181d5f2d117.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c373aedeee3e1e9a514cd280fb2558c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c304bf2f95696c51b5e",
+    "url": "/static/css/main.928b94ec.chunk.css"
+  },
+  {
+    "revision": "cd138592ea2c9f0f886e",
+    "url": "/static/js/2.db78193a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.db78193a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c304bf2f95696c51b5e",
+    "url": "/static/js/main.4f241bd3.chunk.js"
+  },
+  {
+    "revision": "dcbd6308bfa1ef243a46",
+    "url": "/static/js/runtime-main.d5772e23.js"
+  },
+  {
+    "revision": "7af01a39dc6ceb9bc20725fd65020c57",
+    "url": "/static/media/Bathing_Culture500.7af01a39.jpg"
+  },
+  {
+    "revision": "050b2b3b2a50bca19c5eb2ad68ba470a",
+    "url": "/static/media/Big_City_Pipe_Dream500.050b2b3b.jpg"
+  },
+  {
+    "revision": "d5382045f3ce788c9ea7ffdc0c7cde2f",
+    "url": "/static/media/Deep_Dreamliner500.d5382045.jpg"
+  },
+  {
+    "revision": "25b58cc61bd00df34a47bffef24c8bb1",
+    "url": "/static/media/Dive-in_Theatre500.25b58cc6.jpg"
+  },
+  {
+    "revision": "392e6469ea731c65a2aadf5b8efe3319",
+    "url": "/static/media/Drink_And_Dream500.392e6469.jpg"
+  },
+  {
+    "revision": "1abe7af788458a243ac24e230e1140be",
+    "url": "/static/media/Late_Night_Pipe_Dream500.1abe7af7.jpg"
+  },
+  {
+    "revision": "dd7ec8df00bd1a500c8072033fadebea",
+    "url": "/static/media/Sign_of_The_Times500.dd7ec8df.jpg"
+  },
+  {
+    "revision": "fc7d9bf95bf2e8e54a4b46e09b93456b",
+    "url": "/static/media/Sleep_Street_Station500.fc7d9bf9.jpg"
+  },
+  {
+    "revision": "3640f8ec628abc90178210e696fffe2c",
+    "url": "/static/media/Suburban_Shore500.3640f8ec.jpg"
+  },
+  {
+    "revision": "e511e50ec81c5f8708d72f332ed5ae5c",
+    "url": "/static/media/Summer_Games500.e511e50e.jpg"
+  },
+  {
+    "revision": "ed943ed34144ddc326b236066e5b8b21",
+    "url": "/static/media/Sunset_Pipe_Dream500.ed943ed3.jpg"
+  }
+]);
